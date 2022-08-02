7da48ef6f0b56e6ef1ae39eced05618119b0321d
Taylor Savage <taylorsavage93@gmail.com>
2022-08-02 10:32:24 -0400

    finished exercise 1

---
+++ BESTYBUYPRACTICES/IDepartmentRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BESTYBUYPRACTICES
+{
+    internal interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAllDepartments();
+
+        void InsertDepartment(string newDepartmentName);
+
+    }
+}
--- BESTYBUYPRACTICES/Program.cs
+++ BESTYBUYPRACTICES/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BESTYBUYPRACTICES;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+string connString = config.GetConnectionString("DefaultConnection");
+
+IDbConnection conn = new MySqlConnection(connString);
+
+var repo = new DapperDepartmentRepository(conn);
+
+var departments = repo.GetAllDepartments();
+
+
+foreach (var dept in departments)
+{
+    Console.WriteLine($"{dept.DepartmentID} {dept.Name}");
+}
+
+Console.WriteLine("Do you want to add a deparrtment?");
+
+string userResponse = Console.ReadLine();
+
+if (userResponse.ToLower() == "yes")
+{
+    Console.WriteLine("What is the name of you new Department?");
+    userResponse = Console.ReadLine();
+
+    repo.InsertDepartment(userResponse);
+}
+Console.WriteLine("Thank you have a nice day.");
